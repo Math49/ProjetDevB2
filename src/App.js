608589daffe5ebbda8a80dd1a9184464f26a4435
Hockwood <mthsmercier@gmail.com>
2024-01-11 14:43:49 +0100

    Init header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import './App.scss';
+import Header from "./view/header/header";
 
 function App() {
   return (
     <div className="App">
       <h1>ça fonctionne !!</h1>
+      <Header/>
       <Routes>
         <Route path='/' element={""} />
       </Routes>
