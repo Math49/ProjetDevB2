86444729f428704c63f392ab257b6b2340295883
NicolasCode72 <nicolasbellinapro@gmail.com>
2024-01-12 14:33:27 +0100

    h

--- src/components/projetCard/projetCard.jsx
+++ src/components/projetCard/projetCard.jsx
@@ -1,20 +1,34 @@
-import "./projetCard.scss"
+import "./projetCard.scss";
 
-export default function ProjetCard({data}){
+export default function ProjetCard({ data }) {
+  const competences = Object.entries(data.competences).filter((c) => c[1]);
 
+  return (
+    <div className="card">
+        <div className="title-row">
+      <h2 className="card-title">{data.nom}</h2>
+      <span>-</span>
+      <p>nom de l'entreprise</p>
+      </div>
+
+      <div className="texte">
+        <p>Objectifs</p>
+      </div>{" "}
+      <p>{data.objectifs}</p>
+      <div className="texte">
+        <p>Description du projet</p>
+      </div>{" "}
+      <p>{data.description}</p>
+      <div className="texte">
+        <p>Competences</p>
+      </div>
+      <div className="cmp">
+      {competences.map((comp) => (
+        <p key={comp[0]}>{comp[0]}</p>
+      ))}
+      </div>
+    </div>
     
-    const competences = Object.entries(data.competences).filter((c) => c[1])
     
-
-
-    return  <div className="card">
-                <h1 className="card-title">{data.nom}</h1>
-                <p>Concept :</p>
-                <p>{data.description}</p>
-
-                <p>Compétences :</p>
-                {competences.map((comp) =>
-                    <p key={comp[0]}>{comp[0]}</p>
-                )}
-            </div>
+  );
 }
