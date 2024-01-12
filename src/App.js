cb241b38059ad15d2571d0033b892b8b2b953dc8
Hockwood <mthsmercier@gmail.com>
2024-01-12 08:57:33 +0100

    init bdd

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import './App.scss';
 import Header from "./view/header/header";
+import DataDisplay from "./components/test";
 
 function App() {
+
+
   return (
     <div className="App">
       <h1>ça fonctionne !!</h1>
       <Header/>
       <Routes>
-        <Route path='/' element={""} />
+        <Route path='/' element={<DataDisplay/>} />
       </Routes>
     </div>
   );
---
+++ src/components/test.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const DataDisplay = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/data');
+        setData(response.data);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {data.map((item, index) => (
+        <div key={index}>{item.name} | {item.age}</div>
+      ))}
+    </div>
+  );
+}
+
+export default DataDisplay;
