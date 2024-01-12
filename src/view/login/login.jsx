d30e0a45d2adcbd5e9707162f85e50cd57823835
SAMUEL LANDAIS <samuel.landais.pro@gmail.com>
2024-01-12 14:03:22 +0100

    a

---
+++ src/view/login/login.jsx
@@ -0,0 +1,14 @@
+import './login.scss'
+
+export default function Login(){
+    
+    return (
+
+        <main>
+            <img src="espl logo.png" alt="logo ESPL" />
+            <h1>Page login</h1>
+        </main>
+
+    )
+
+}
