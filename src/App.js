2e34e3d6a5786ff57ab749dec62955a8ed6994df
Samy ZIANE <samy.ziane-pro@outlook.com>
2024-01-12 18:46:40 +0100

    debut page single project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useParams, useNavigation, useNavigate, useLocation } from "react-router-dom";
 import './App.scss';
 import Header from "./view/header/header";
 import DataDisplay from "./components/test";
 import ListProjet from "./view/listProjet/listProjet";
+import SingleProject from "./components/singleProject/singleProject";
+
+
+// const RenderTest = () => {
+//   const { projectId } = useParams();
+//   const location = useLocation();
+
+//   console.log(projectId);
+//   return (
+//     <div>
+//       <h1>{projectId}</h1>
+//       {location?.state?.project && (
+//         <div>{location.state.project.name}</div>
+//       )}
+//       {location?.state?.gender && (
+//         <div>{location.state.gender.toString()}</div>  
+//       )}
+      
+//     </div>
+//   )
+// }
+// const ProjectsScreen = () => {
+//   const navigate = useNavigate();
+
+//   const navigateToProject = (project) => {
+//     navigate(`/projet/${project.id}`, { state: {project: project} })
+
+//   }
+
+//   return (
+//     <div>
+//       <button onClick={() => navigateToProject(project)}>Je veux voir mon projet</button>
+//     </div>
+//   )
+// }
 
 function App() {
 
@@ -12,6 +47,9 @@ function App() {
       <Header/>
       <Routes>
         <Route path='/' element={<ListProjet/>} />
+        {/* <Route path='/projets/' element={<ProjectsScreen/>} />
+        <Route path='/projet/:projectId' element={<RenderTest/>} /> */}
+        <Route path='/projets' element={<SingleProject/>} />
       </Routes>
     </div>
   );
---
+++ src/components/singleProject/singleProject.jsx
@@ -0,0 +1,80 @@
+import "./singleProject.scss";
+
+export default function SingleProject() {
+  return (
+    <div className="single-wrapper global-wrapper">
+        <div className="single-left">
+                <div className="single-left__btn">
+                    <a href="" className="btn bg-orange">
+                        Retourner a la liste des projets
+                    </a>
+                    <a href="" className="btn bg-blue">
+                        Projet terminé
+                    </a>
+                </div>
+            <div className="single-project">
+                <div className="single-project__title">
+                        <h2 className="project-title">Projet lorem ipsum</h2>
+                        <span>-</span>
+                        <h2 className="society-name">Entreprise</h2>
+                </div>
+                <div className="single-project__content">
+                    <div className="single-project__objectif">
+                        <p className="submenu-title">Objectifs</p>
+                        <p>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde
+                            iste quis veniam quia totam. Tempora nemo nesciunt enim odit
+                            soluta assumenda modi temporibus dignissimos possimus aut.
+                            Expedita consectetur ratione incidunt.
+                        </p>
+                    </div>
+                    <div className="single-project__description">
+                        <p className="submenu-title">Description du projet</p>
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                            Ducimus debitis laudantium porro vitae nobis at architecto
+                            fugiat distinctio aspernatur odit! Totam tenetur quisquam ab
+                            odit aliquid perferendis maxime inventore sed? Nisi accusantium
+                            a voluptates quasi aliquid consectetur doloribus, praesentium in
+                            mollitia quibusdam accusamus quidem, consequatur minus ipsa
+                            sequi? Illo doloremque fuga dolore eos. Eos corrupti eaque, non
+                            doloremque rem ex! Ipsum earum obcaecati quos dignissimos ex
+                            dolore labore. A explicabo deleniti ipsum voluptatem
+                            consequuntur consectetur repellendus odit nostrum est
+                            reprehenderit asperiores voluptas, ex aliquam sed eaque rem
+                            beatae tempora veritatis.
+                        </p>
+                    </div>
+                    <div className="single-project__participant">
+                        <p className="submenu-title">Participants</p>
+                        <ul>
+                            <li>
+                                Samy Ziane<p className="is-chef-project">Chef de projet</p>
+                                <p className="competence">Développement Web</p>
+                            </li>
+                            <li>
+                                Mathis Mercier<p className="competence">Développement Web</p>
+                            </li>
+                            <li>
+                                Samuel Landais<p className="competence">Développement Web</p>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="single-project__competences">
+                        <p className="submenu-title">Compétences requises</p>
+                        <div className="competences">
+                            <p className="competence">Graphisme</p>
+                            <p className="competence">Développement Web</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="single-right">
+            <div className="single-historic">
+                <div className="submenu-title">Historique de la progression</div>
+            </div>
+        </div>
+    </div>
+  );
+}
