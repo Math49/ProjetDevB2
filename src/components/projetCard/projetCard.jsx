e0ed7c368e179f32f3f3810e493d21d63efae118
Hockwood <mthsmercier@gmail.com>
2024-02-05 10:02:56 +0100

    Filtrage projets

--- src/components/projetCard/projetCard.jsx
+++ src/components/projetCard/projetCard.jsx
@@ -1,34 +1,32 @@
 import "./projetCard.scss";
 
 export default function ProjetCard({ data }) {
-  const competences = Object.entries(data.competences).filter((c) => c[1]);
+    const competences = Object.entries(data.competences).filter((c) => c[1]);
 
-  return (
-    <div className="card">
-        <div className="title-row">
-      <h2 className="card-title">{data.nom}</h2>
-      <span>-</span>
-      <p>nom de l'entreprise</p>
-      </div>
 
-      <div className="texte">
-        <p>Objectifs</p>
-      </div>{" "}
-      <p>{data.objectifs}</p>
-      <div className="texte">
-        <p>Description du projet</p>
-      </div>{" "}
-      <p>{data.description}</p>
-      <div className="texte">
-        <p>Competences</p>
-      </div>
-      <div className="cmp">
-      {competences.map((comp) => (
-        <p key={comp[0]}>{comp[0]}</p>
-      ))}
-      </div>
+    return (
+        <div className="card">
+            <div className="title-row">
+                <h2 className="card-title">{data.nom}</h2>
+                <span>-</span>
+                <p>{data.auteur}</p>
+            </div>
+            <div className="objectif row">
+                <h3>Objectifs</h3>
+                <p>{data.objectifs}</p> 
+            </div>
+            <div className="desc row">
+                <h3>Description du projet</h3>
+                <p>{data.description}</p>
+            </div>
+            <div className="compe row">
+                <h3>Compétences</h3>
+                <div className="cmp">
+                {competences.map((comp) => (
+                    <p key={comp[0]}>{comp[0]}</p>
+                ))}
+            </div>
+        </div>
     </div>
-    
-    
-  );
+    );
 }
--- src/view/listProjet/listProjet.jsx
+++ src/view/listProjet/listProjet.jsx
@@ -1,31 +1,87 @@
-import "./listProjet.scss"
-import ProjetCard from "../../components/projetCard/projetCard"
+import "./listProjet.scss";
+import ProjetCard from "../../components/projetCard/projetCard";
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-
+import Checkbox from "./checkbox";
 
 export default function ListProjet() {
+    const [data, setData] = useState([]);
+    const [q, setQ] = useState("");
+    const [categories, setCategories] = useState({
+        design: false,
+        dev: false
+    });
 
-    const data = [{competences:{"test1.1":false,"test1":true},objectifs:"test1",concept:"test1",description:"test1",id:1,nom:"test1"},{objectifs:"test2",concept:"test2",description:"test2",id:2,nom:"test2",competences:{"test2":true,"test2.1":true}}]
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:3000/importProjet');
+                setData(response.data);
+            } catch (error) {
+                console.error("Error fetching data: ", error);
+            }
+        };
 
-    // const [data, setData] = useState([]);
+        fetchData();
+    }, []);
 
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //     try {
-    //         const response = await axios.get('http://localhost:3000/importProjet');
-    //         setData(response.data);
-    //     } catch (error) {
-    //         console.error("Error fetching data: ", error);
-    //     }
-    //     };
+    const handleChange = e => {
+        const { name, checked } = e.target;
 
-    //     fetchData();
-    // }, []);
+        setCategories(prev => ({
+            ...prev,
+            [name]: checked
+        }));
+    };
 
-    return  <div className="card-container">
-                {data.map((obj) =>
-                    <ProjetCard data={obj}/>
+    const filterProjects = projects => {
+        return projects.filter(project => {
+            const matchesName = q ? project.nom.toLowerCase().includes(q.toLowerCase()) : true;
+            const competences = project.competences || {};
+            const checkedCategories = Object.entries(categories).filter(([_, value]) => value).map(([key]) => key);
+            const matchesCategories = checkedCategories.length > 0 ? checkedCategories.some(category => competences[category]) : true;
+
+            return matchesName && matchesCategories;
+        });
+    };
+
+    return (
+        <div className="listing-projet">
+            <div className="filtre-menu">
+                <h2>Filtrer les projets</h2>
+                <div className="search-wrapper">
+                    <label htmlFor="search-form">
+                        <input
+                            type="search"
+                            name="search-form"
+                            id="search-form"
+                            className="search-input"
+                            placeholder="Search for..."
+                            value={q}
+                            onChange={(e) => setQ(e.target.value)}
+                        />
+                    </label>
+                </div>
+                <Checkbox
+                    id="design"
+                    title="Design"
+                    name="design"
+                    checked={categories.design}
+                    handleChange={handleChange}
+                />
+                <Checkbox
+                    id="dev"
+                    title="Dev"
+                    name="dev"
+                    checked={categories.dev}
+                    handleChange={handleChange}
+                />
+            </div>
+            <div className="card-container">
+                {filterProjects(data).map((obj) => 
+                    <ProjetCard key={obj.id} data={obj}/>
                 )}
             </div>
+        </div>
+    );
 }
