f0b8965aca251dd989e980d5f773e4c87ead0c6e
SAMUEL LANDAIS <samuel.landais.pro@gmail.com>
2024-02-06 16:07:10 +0100

    comptes et add account

--- src/components/dashboardOnglet/comptes.jsx
+++ src/components/dashboardOnglet/comptes.jsx
@@ -4,23 +4,15 @@ export default function Comptes() {
     return (
         <div className="comptes">
 
-            <AdminUserCard/>
-            <AdminUserCard/>
-            <AdminUserCard/>
-            <AdminUserCard/>
-            <AdminUserCard/>
-            <AdminUserCard/>
-            <AdminUserCard/>
-            <AdminUserCard/>
-            <AdminUserCard/>
-            <AdminUserCard/>
-            <AdminUserCard/>
-            <AdminUserCard/>
-            <AdminUserCard/>
-            <AdminUserCard/>
-            <AdminUserCard/>
-            <AdminUserCard/>
-            <AdminUserCard/>
+            <a href="/register" className="add-account">
+                <img src="../../../add-user.png" alt="Ajouter un utilisateur" className="add-user-icon" />
+                Ajouter un compte
+            </a>
+
+            <div className="comptes-list">
+                <AdminUserCard/>
+            </div>
+
 
         </div>
     )
