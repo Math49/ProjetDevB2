c048399f728ce02b29f30611cfa5d598bf5ea198
Adrien Conrard <adrien.conrard@gmail.com>
2024-02-06 11:00:18 +0100

    register all

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.scss';
 import Header from "./view/header/header";
 import ListProjet from "./view/listProjet/listProjet";
 import Login from "./view/login/login";
+import Register from "./view/register/register";
 
 function App() {
 
@@ -12,7 +13,7 @@ function App() {
       <Header/>
       <Routes>
         <Route path='/' element={<ListProjet/>} />
-        <Route path='/login' element={<Login/>} />
+        <Route path='/register' element={<Register/>} />
       </Routes>
     </div>
   );
