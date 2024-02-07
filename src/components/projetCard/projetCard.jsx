bb02a7ac5d66865a7ea0075b7902b7cb1d535215
SAMUEL LANDAIS <samuel.landais.pro@gmail.com>
2024-02-07 13:36:36 +0100

    dashboard styling

--- src/components/projetCard/projetCard.jsx
+++ src/components/projetCard/projetCard.jsx
@@ -3,6 +3,17 @@ import "./projetCard.scss";
 export default function ProjetCard({ data }) {
     const competences = Object.entries(data.competences).filter((c) => c[1]);
 
+    const getColorById = id => {
+        const colors = {
+          dev: '#C42D2D',
+          graphisme: '#2D72C4',
+          marketing: '#9D2DC4',
+          webdesign: '#1DBA23',
+          communication: '#C84DDC',
+        };
+        return colors[id] || '#defaultColor';
+      };
+      
 
     return (
         <div className="card">
@@ -22,10 +33,12 @@ export default function ProjetCard({ data }) {
             <div className="compe row">
                 <h3>Compétences</h3>
                 <div className="cmp">
-                {competences.map((comp) => (
-                    <p key={comp[0]}>{comp[0]}</p>
-                ))}
-            </div>
+                    {competences.map(comp => (
+                        <span key={comp[0]} style={{ backgroundColor: getColorById(comp[0]) }}>
+                        {comp[0]}
+                        </span>
+                    ))}
+                </div>
             <div>
                 
             </div>
